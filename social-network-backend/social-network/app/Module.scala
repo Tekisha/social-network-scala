d77bc2bba6ed2795af7e3605700e7a58e746b28d
Teodor Vidakovic <teodorv22@gmail.com>
2024-09-03 15:39:26 +0200

    delete unnecessary files

--- social-network-backend/social-network/app/Module.scala
+++
@@ -1,8 +0,0 @@
-import com.google.inject.AbstractModule
-import java.time.Clock
-
-class Module extends AbstractModule {
-  override def configure() = {
-    bind(classOf[ApplicationStartUp]).asEagerSingleton()
-  }
-}
