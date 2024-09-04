789f8742a28fd8d523697d3a59203c387eb6945f
Teodor Vidakovic <teodorv22@gmail.com>
2024-09-04 15:12:09 +0200

    feat: add getAll test cases

--- social-network-backend/social-network/test/controllers/UserControllerSpec.scala
+++ social-network-backend/social-network/test/controllers/UserControllerSpec.scala
@@ -3,7 +3,7 @@ import play.api.libs.json.Json
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, Injecting}
 import controllers.UserController
-import play.api.mvc.Headers
+import play.api.mvc.{AnyContentAsEmpty, Headers}
 import play.api.libs.json.{Json, JsObject}
 
 class UserControllerSpec extends TestBase {
@@ -63,6 +63,52 @@ class UserControllerSpec extends TestBase {
 
       status(loginResult) mustBe UNAUTHORIZED
       contentAsJson(loginResult) mustBe Json.obj("message" -> "Invalid credentials")
+    }
+  }
+
+  "UserController GET getAllUsers" should {
+
+    "return a list of users when authenticated, including two registered users" in {
+      //register the first user
+      val registrationData1 = Json.obj("username" -> "testuser1", "password" -> "password123")
+      val registrationRequest1: FakeRequest[JsObject] = FakeRequest(POST, "/register")
+        .withBody(registrationData1)
+      val registrationResult1 = route(app, registrationRequest1).get
+      status(registrationResult1) mustBe CREATED
+
+      // Register the second user
+      val registrationData2 = Json.obj("username" -> "testuser2", "password" -> "password456")
+      val registrationRequest2: FakeRequest[JsObject] = FakeRequest(POST, "/register")
+        .withBody(registrationData2)
+      val registrationResult2 = route(app, registrationRequest2).get
+      status(registrationResult2) mustBe CREATED
+
+      //log in as the first user to get the token
+      val loginData = Json.obj("username" -> "testuser1", "password" -> "password123")
+      val loginRequest: FakeRequest[JsObject] = FakeRequest(POST, "/login")
+        .withBody(loginData)
+      val loginResult = route(app, loginRequest).get
+      status(loginResult) mustBe OK
+      val token = (contentAsJson(loginResult) \ "token").as[String]
+
+      //fetch the list of users
+      val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(GET, "/users")
+        .withHeaders("Authorization" -> s"Bearer $token")
+      val result = route(app, request).get
+
+      status(result) mustBe OK
+      val users = contentAsJson(result).as[Seq[JsObject]]
+
+      users.nonEmpty mustBe true
+      users.map(user => (user \ "username").as[String]) must contain allOf ("testuser1", "testuser2")
+    }
+
+    "return 401 Unauthorized when no token is provided" in {
+      val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(GET, "/users")
+
+      val result = route(app, request).get
+
+      status(result) mustBe UNAUTHORIZED
     }
   }
 }
