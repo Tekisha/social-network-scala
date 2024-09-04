6724b80b3c021f3846796df5ebed4e01633aa199
Teodor Vidakovic <teodorv22@gmail.com>
2024-09-04 14:46:11 +0200

    feat: update test base

--- social-network-backend/social-network/test/base/TestBase.scala
+++ social-network-backend/social-network/test/base/TestBase.scala
@@ -2,24 +2,41 @@ package base
 
 import org.scalatestplus.play._
 import play.api.db.evolutions.Evolutions
-import play.api.db.Database
+import slick.jdbc.JdbcBackend.Database
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import models.User
 import utils.JwtUtils
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.db.{DBApi}
 
 abstract class TestBase extends PlaySpec with BeforeAndAfterEach with GuiceOneServerPerSuite {
 
-  private lazy val db: Database = app.injector.instanceOf[Database]
+  private lazy val db: Database = Database.forConfig("slick.dbs.default")
+  lazy val dbApi: DBApi = app.injector.instanceOf[DBApi]
+
+  override def fakeApplication() = new GuiceApplicationBuilder()
+    .configure(
+      "slick.dbs.default.profile" -> "slick.jdbc.MySQLProfile$",
+      "slick.dbs.default.db.driver" -> "com.mysql.cj.jdbc.Driver",
+      "slick.dbs.default.db.url" -> "jdbc:mysql://localhost:3306/social_network",
+      "slick.dbs.default.db.user" -> "root",
+      "slick.dbs.default.db.password" -> "admin",
+      "play.evolutions.enabled" -> "true",
+      "play.evolutions.autoApply" -> "true",
+      "play.filters.enabled" -> Seq()   // Disable the AllowedHostsFilter
+    )
+    .build()
+
   val token: String = JwtUtils.createToken(1, "pera@email.com", 7)
 
   override def beforeEach(): Unit = {
     super.beforeEach()
-    Evolutions.applyEvolutions(db)
+    Evolutions.applyEvolutions(dbApi.database("default"))
   }
 
   override def afterEach(): Unit = {
-    Evolutions.cleanupEvolutions(db)
+    Evolutions.cleanupEvolutions(dbApi.database("default"))
     super.afterEach()
   }
 }
