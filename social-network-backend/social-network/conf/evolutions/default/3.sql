8377ac3268fcdbbe27bd326109949272a978bdf1
Teodor Vidakovic <teodorv22@gmail.com>
2024-09-06 16:40:10 +0200

    feat: check if friendship is created after accepting the request, and fix initial data insertion inconsistency

--- social-network-backend/social-network/conf/evolutions/default/3.sql
+++ social-network-backend/social-network/conf/evolutions/default/3.sql
@@ -9,8 +9,8 @@ CREATE TABLE friend_requests (
                                  FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-INSERT INTO friend_requests (requester_id, receiver_id, status) VALUES (1, 2, 'pending');
-INSERT INTO friend_requests (requester_id, receiver_id, status) VALUES (2, 3, 'accepted');
+INSERT INTO friend_requests (requester_id, receiver_id, status) VALUES (1, 2, 'accepted');
+INSERT INTO friend_requests (requester_id, receiver_id, status) VALUES (2, 3, 'pending');
 INSERT INTO friend_requests (requester_id, receiver_id, status) VALUES (3, 1, 'rejected');
 
 # --- !Downs
