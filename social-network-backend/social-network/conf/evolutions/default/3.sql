ce5407fc962783c7fb4b1545c945b870644e4688
Teodor Vidakovic <teodorv22@gmail.com>
2024-09-06 10:27:35 +0200

    feat: add new migration

---
+++ social-network-backend/social-network/conf/evolutions/default/3.sql
@@ -0,0 +1,17 @@
+# --- !Ups
+CREATE TABLE friend_requests (
+                                 id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                 requester_id INT UNSIGNED NOT NULL,
+                                 receiver_id INT UNSIGNED NOT NULL,
+                                 status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+                                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                 FOREIGN KEY (requester_id) REFERENCES users(id) ON DELETE CASCADE,
+                                 FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+INSERT INTO friend_requests (requester_id, receiver_id, status) VALUES (1, 2, 'pending');
+INSERT INTO friend_requests (requester_id, receiver_id, status) VALUES (2, 3, 'accepted');
+INSERT INTO friend_requests (requester_id, receiver_id, status) VALUES (3, 1, 'rejected');
+
+# --- !Downs
+DROP TABLE IF EXISTS friend_requests;
