9ecb7942d4ce71afbe1a8b64552467ae900d29d8
Teodor Vidakovic <teodorv22@gmail.com>
2024-09-10 10:26:27 +0200

    feat: show active nav-item

--- social-network-frontend/social-network/src/components/navbar/navbar.jsx
+++ social-network-frontend/social-network/src/components/navbar/navbar.jsx
@@ -3,6 +3,11 @@ import './Navbar.css';
 import logo from '/src/assets/logo.png';
 
 function Navbar() {
+
+    const getActiveClass = (path) => {
+        return window.location.pathname === path ? "active-nav-item" : "";
+    };
+    
     return (
             <header className="navbar">
                 <div className="logo-container">
@@ -10,9 +15,9 @@ function Navbar() {
                     <h2 className="site-title">SocialNetwork</h2>
                 </div>
                 <nav>
-                    <a href="/home"><i className="fas fa-home"></i> Home</a>
-                    <a href="/profile"><i className="fas fa-user"></i> Profile</a>
-                    <a href="/search"><i className="fas fa-search"></i> Search</a>
+                    <a href="/home" className={getActiveClass("/home")}><i className="fas fa-home"></i> Home</a>
+                    <a href="/profile" className={getActiveClass("/profile")}><i className="fas fa-user"></i> Profile</a>
+                    <a href="/search" className={getActiveClass("/search")}><i className="fas fa-search"></i> Search</a>
                     <a href="/login"><i className="fas fa-sign-out-alt"></i> Logout</a>
                 </nav>
             </header>
