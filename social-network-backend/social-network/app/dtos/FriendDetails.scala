5a45edc2ad93c292158baf1d3e6eb1ba97e08af0
Teodor Vidakovic <teodorv22@gmail.com>
2024-09-16 11:50:48 +0200

    feat: return profile photo in friend details response

--- social-network-backend/social-network/app/dtos/FriendDetails.scala
+++ social-network-backend/social-network/app/dtos/FriendDetails.scala
@@ -2,7 +2,7 @@ package dtos
 
 import play.api.libs.json.{Json, OFormat}
 
-case class FriendDetails(id: Option[Int], friendId: Int, username: String)
+case class FriendDetails(id: Option[Int], friendId: Int, username: String, profilePhoto: String)
 
 object FriendDetails {
   implicit val format: OFormat[FriendDetails] = Json.format[FriendDetails]
